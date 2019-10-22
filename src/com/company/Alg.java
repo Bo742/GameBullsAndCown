18c1618e5014e62e41fe76c9e3b2878f0891dc38
Timofey <bo742@mail.ru>
2019-10-22 22:48:47 +0800

    Version 1.1(Fixed repeat number)

--- src/com/company/Alg.java
+++ src/com/company/Alg.java
@@ -1,138 +1,187 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Alg {
     private static Scanner in = new Scanner(System.in);
     private static Sequence tryFoundSequence;
     private static int lengthUserSequence = 0;
-    private static int rightNumberInSequence = 0;
-    private static int rightNumberInSequenceOnPosition = 0;
+    private static int rightNumberInSequenceOld = 0;
+    private static int rightNumberInSequenceCurrent = 0;
+    private static int rightNumberInSequenceOnPositionOld = 0;
+    private static int rightNumberInSequenceOnPositionCurrent = 0;
     private static int maxNumberInSequence=0;
     private static int currentPositionInSequence = 0;
-    private static int numberCurrentPositionForBackUp = 0;
-    private static String answer = "";
+    private static int memoryNumber = 0;
+    private static ArrayList<Integer> memory = new ArrayList<>();
     private static boolean[] rightPosition ;
 
     public Alg(int length){
-        lengthUserSequence=length;
-        rightPosition = new boolean[lengthUserSequence];
+        lengthUserSequence = length;
         tryFoundSequence = new Sequence(lengthUserSequence);
+        rightPosition = new boolean[lengthUserSequence];
         for (int i = 0; i < lengthUserSequence ; i++) {
-            tryFoundSequence.setNumberSequence(i,i);
-            maxNumberInSequence=i;
             rightPosition[i] = false;
         }
-
-        firstIteration();
-        otherIteration();
     }
 
-    public void otherIteration(){
-        if(rightNumberInSequenceOnPosition == lengthUserSequence){
-            answer = tryFoundSequence.getSequence();
+    private void otherTry(){
+        if(rightNumberInSequenceCurrent == lengthUserSequence){
+            if(rightNumberInSequenceOnPositionCurrent != lengthUserSequence){
+                sortSequence();
+            }
         }
         else{
-            if(rightNumberInSequence != lengthUserSequence){
-                numberCurrentPositionForBackUp = tryFoundSequence.getNumberSequence(currentPositionInSequence);
-                int num = (maxNumberInSequence + 1)%10;
-                tryFoundSequence.setNumberSequence(num,currentPositionInSequence);
-                System.out.println(tryFoundSequence.getSequence());
-                int rs = in.nextInt();
-                int rsp = in.nextInt();
-                if(rs>rightNumberInSequence){
-                    maxNumberInSequence++;
-                    rightNumberInSequence=rs;
-                    if(rsp>rightNumberInSequenceOnPosition){
-                        rightNumberInSequenceOnPosition = rsp;
-                        rightPosition[currentPositionInSequence] = true;
+            changeNumberInSequence();
+            iteration();
+            if(rightNumberInSequenceCurrent > rightNumberInSequenceOld){
+                rightNumberInSequenceOld = rightNumberInSequenceCurrent;
+                if(rightNumberInSequenceOnPositionCurrent > rightNumberInSequenceOnPositionOld){
+                    rightPosition[currentPositionInSequence] = true;
+                    rightNumberInSequenceOnPositionOld = rightNumberInSequenceOnPositionCurrent;
+                }
+                currentPositionInSequence++;
+                int k = memory.size();
+                if(k>0){
+                    for (int i = k-1; i >=0; i--) {
+                        memory.remove(i);
                     }
-                    currentPositionInSequence++;
+                }
+            }
+            else{
+                if(rightNumberInSequenceCurrent == rightNumberInSequenceOld){
+                    if(rightNumberInSequenceOnPositionCurrent > rightNumberInSequenceOnPositionOld){
+                        rightPosition[currentPositionInSequence] = true;
+                        rightNumberInSequenceOnPositionOld = rightNumberInSequenceOnPositionCurrent;
+                        currentPositionInSequence++;
+                        for (int i = 0; i < memory.size() ; i++) {
+                            tryFoundSequence.setNumberSequence(memory.get(i),currentPositionInSequence);
+                            currentPositionInSequence++;
+                        }
+                        memory.remove(memory);
+                    }
+                    else{
+                        if(rightNumberInSequenceOnPositionCurrent < rightNumberInSequenceOnPositionOld){
+                            tryFoundSequence.setNumberSequence(memoryNumber, currentPositionInSequence);
+                            currentPositionInSequence++;
+                            for (int i = 0; i < memory.size() ; i++) {
+                                tryFoundSequence.setNumberSequence(memory.get(i),currentPositionInSequence);
+                                currentPositionInSequence++;
+                            }
+                            memory.remove(memory);
+                        }
+                        else{
+                            memory.add(memoryNumber);
+                        }
+                    }
                 }
                 else{
-                    if(rs<rightNumberInSequence){
-                        tryFoundSequence.setNumberSequence(numberCurrentPositionForBackUp,currentPositionInSequence);
-                        maxNumberInSequence++;
-                        if(rsp<rightNumberInSequenceOnPosition){
-                            rightPosition[currentPositionInSequence] = true;
-                        }
+                    memory.add(memoryNumber);
+                    for (int i = 0; i < memory.size() ; i++) {
+                        tryFoundSequence.setNumberSequence(memory.get(i),currentPositionInSequence);
                         currentPositionInSequence++;
                     }
-                    if(rs==rightNumberInSequence){
-                        tryFoundSequence.setNumberSequence(numberCurrentPositionForBackUp,currentPositionInSequence);
-                        maxNumberInSequence++;
-                    }
+                    memory.remove(memory);
                 }
-                otherIteration();
             }
-            if(rightNumberInSequence==lengthUserSequence){
-                int i=0;
-                int j=i+1;
-                while (rightNumberInSequence != rightNumberInSequenceOnPosition) {
+            otherTry();
+        }
+    }
+
+    private void sortSequence() {
+        int i = 0;
+        int j = i + 1;
+        while (rightNumberInSequenceCurrent != rightNumberInSequenceOnPositionCurrent) {
+            if (rightPosition[i]) {
+                i++;
+            } else {
+                if (j == lengthUserSequence) {
+                    i++;
                     if (rightPosition[i]) {
                         i++;
+                    }
+                    j = i + 1;
+                }
+                if (rightPosition[j]) {
+                    j++;
+                } else {
+                    if (i == j) {
+                        j++;
                     } else {
-                        if (j == lengthUserSequence) {
-
+                        toSwap(i, j);
+                        iteration();
+                        if (rightNumberInSequenceOnPositionCurrent < rightNumberInSequenceOnPositionOld) {
+                            toSwap(i, j);
                             i++;
-                            if (rightPosition[i]) {
-                                i++;
-                            }
                             j = i + 1;
-                        }
-                        if (rightPosition[j]) {
-                            j++;
                         } else {
-                            if (i == j) {
-                                j++;
-                            } else {
-                                toSwap(i, j);
-                                System.out.println(tryFoundSequence.getSequence());
-                                int rNS = in.nextInt();
-                                int rNSP = in.nextInt();
-                                if (rNSP < rightNumberInSequenceOnPosition) {
-                                    toSwap(i, j);
-                                    i++;
-                                } else {
-                                    rightNumberInSequenceOnPosition = rNSP;
-                                }
-                                j++;
-                            }
+                            rightNumberInSequenceOnPositionOld = rightNumberInSequenceOnPositionCurrent;
+                            j++;
                         }
+
                     }
                 }
-                otherIteration();
             }
 
         }
     }
 
-    public void toSwap(int first,int second){
+    private void toSwap(int first,int second){
         int swap = tryFoundSequence.getNumberSequence(first);
         tryFoundSequence.setNumberSequence(tryFoundSequence.getNumberSequence(second),first);
         tryFoundSequence.setNumberSequence(swap,second);
     }
 
-    public void createNewSequence(){
-        int mx = maxNumberInSequence;
-        for (int i = mx+1; i < lengthUserSequence+mx+1 ; i++) {
-            tryFoundSequence.setNumberSequence(i,i-mx-1);
-            maxNumberInSequence=i;
-        }
-        firstIteration();
+    private void changeNumberInSequence(){
+        memoryNumber = tryFoundSequence.getNumberSequence(currentPositionInSequence);
+        tryFoundSequence.setNumberSequence(maxNumberInSequence , currentPositionInSequence);
+        maxNumberInSequence++;
     }
 
-    public void firstIteration(){
-        System.out.println(tryFoundSequence.getSequence());
-        rightNumberInSequence = in.nextInt();
-        rightNumberInSequenceOnPosition = in.nextInt();
-        if(rightNumberInSequence==0){
-            createNewSequence();
+    private void firstTry(){
+        for (int i = 0; i < lengthUserSequence ; i++) {
+            tryFoundSequence.setNumberSequence(i, i);
         }
+        maxNumberInSequence = lengthUserSequence;
+        iteration();
+        if(rightNumberInSequenceCurrent==0){
+            changeTrySequence();
+        }
+        else{
+            rightNumberInSequenceOld = rightNumberInSequenceCurrent;
+            rightNumberInSequenceOnPositionOld = rightNumberInSequenceOnPositionCurrent;
+        }
+    }
+
+    private void changeTrySequence(){
+        for (int i = maxNumberInSequence; i < lengthUserSequence + maxNumberInSequence ; i++) {
+            tryFoundSequence.setNumberSequence(i, i-maxNumberInSequence);
+        }
+        maxNumberInSequence += lengthUserSequence;
+        iteration();
+        if(rightNumberInSequenceCurrent==0){
+            changeTrySequence();
+        }
+        else{
+            rightNumberInSequenceOld = rightNumberInSequenceCurrent;
+            rightNumberInSequenceOnPositionOld = rightNumberInSequenceOnPositionCurrent;
+        }
+    }
+
+    private void iteration(){
+        System.out.println(tryFoundSequence.getSequence());
+        rightNumberInSequenceCurrent = in.nextInt();
+        rightNumberInSequenceOnPositionCurrent = in.nextInt();
     }
 
     public String getAnswer(){
-        return answer;
+        firstTry();
+        otherTry();
+        return tryFoundSequence.getSequence();
     }
+
+
+
 
 }
